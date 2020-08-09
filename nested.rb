27090953e1cae9a53f217b179c90d80525244e74
daniel campo <campo_daniel94@yahoo.com>
2020-08-09 15:05:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,10 +73,6 @@ end
 
 	programmer_hash = 
  		{
- 		  :yukihiro_matsumoto => {
-        :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-      }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
